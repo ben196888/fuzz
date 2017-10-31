391c35eccb20b921cf88425abca770f6f1d3fbdf
Ben Liu <ben.liu@tabcorp.com.au>
2017-10-31 13:20:33 +1100

    Refactor getRatios function to return object instead of string.

--- src/getRatios.js
+++ src/getRatios.js
@@ -1,14 +1,13 @@
 import fuzz from 'fuzzball';
 
 function getRatios(string1, string2) {
-    const sr = fuzz.ratio(string1, string2);
-    const pr = fuzz.partial_ratio(string1, string2);
-    const tst = fuzz.token_set_ratio(string1, string2);
-    const tsr = fuzz.token_sort_ratio(string1, string2);
-    const d = fuzz.distance(string1, string2);
+  const sr = fuzz.ratio(string1, string2);
+  const pr = fuzz.partial_ratio(string1, string2);
+  const tst = fuzz.token_set_ratio(string1, string2);
+  const tsr = fuzz.token_sort_ratio(string1, string2);
+  const d = fuzz.distance(string1, string2);
 
-    const result = `SR: ${sr}, PR: ${pr}, TST: ${tst}, TSR: ${tsr}, D: ${d}`;
-    return result;
+  return { sr, pr, tst, tsr, d };
 };
 
 export default getRatios;
