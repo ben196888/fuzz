57f10f0d95f063bc00f6ad26399f9b4350898f3f
Ben Liu <ben.liu@tabcorp.com.au>
2017-11-02 15:01:59 +1100

    Refactor for tabs browsing

--- src/App.js
+++ src/App.js
@@ -1,130 +1,20 @@
 import React, { Component } from 'react';
-import getRatios from './getRatios';
+import Comparasion from './Comparasion';
+import DisplayDatas from './DisplayDatas';
 import logo from './logo.svg';
 import './App.css';
-
-const KEYS = ['string1', 'string2'];
-const DEFAULT_COM_OBJ = KEYS.reduce((arr, k) => ({...arr, k: ''}), {});
-
-const DEFAULT_PARAMETERS = {
-  sr: '0',
-  pr: '0',
-  tst: '100',
-  tsr: '0',
-  d: '0',
-};
-
-const DEFAULT_THRESHOLD = {
-  threshold: '90',
-};
 
 class App extends Component {
 
   constructor() {
     super();
-    this.getParamInputs = this.getParamInputs.bind(this);
-    this.getThresholdInput = this.getThresholdInput.bind(this);
-    this.getComparison = this.getComparison.bind(this);
-
     this.state = {
-      list: [DEFAULT_COM_OBJ],
-      ...DEFAULT_PARAMETERS,
-      ...DEFAULT_THRESHOLD,
+      tab: 0,
     };
   }
 
-  getParamInputs() {
-    return Object.keys(DEFAULT_PARAMETERS).map((k) => {
-      return (
-        <span key={k}>
-          <label>{k}: {this.state[k]}</label>
-          <input
-            type="range"
-            min="0"
-            max="100"
-            step="5"
-            value={this.state[k]}
-            onChange={(e) => {
-              e.stopPropagation();
-              const value = e.target.value.trim();
-              this.setState((prevState) => {
-                return {
-                  ...prevState,
-                  [k]: value,
-                };
-              })
-            }}
-          />
-        </span>
-      )
-    });
-  }
-
-  getThresholdInput() {
-    return (
-      <span>
-        <label>Threshold</label>
-        <input
-          type="number"
-          min="0"
-          max="100"
-          step="1"
-          value={this.state.threshold}
-          onChange={(e) => {
-            e.stopPropagation();
-            const value = e.target.value.trim();
-            this.setState((prevState) => {
-              return {
-                ...prevState,
-                threshold: value,
-              };
-            })
-          }}
-        />
-      </span>
-    );
-  }
-
-  getComparison(com_obj, index) {
-    const { string1, string2 } = com_obj;
-    const inputs = KEYS.map((k) => (
-      <div key={`${index}_${k}`}>
-        <label>{k}</label>
-        <input className="Input-compare" onChange={(e) => {
-          e.stopPropagation();
-          const value = e.target.value.trim();
-          const list = this.state.list;
-          list[index] = {
-            ...list[index],
-            [k]: value,
-          };
-          this.setState({ list });
-        }} />
-      </div>
-    ));
-    const ratios = getRatios(string1, string2);
-    const rate = Object.keys(DEFAULT_PARAMETERS).reduce((sum, k) => {
-      const a = Number(this.state[k]);
-      const b = Number(ratios[k]);
-      return sum + a * b;
-    }, 0);
-    const mark = rate / 100;
-    const matchDisplay = (mark >= this.state.threshold) ? 'Match' : 'Not Match';
-
-    return (
-      <li className="Li-compare" key={index}>
-        Comparsion #{index}, {matchDisplay}, Mark: {mark}
-        {inputs}
-      </li>
-    );
-  }
-
   render() {
-
-    const paramInputs = this.getParamInputs();
-    const thresholdInput = this.getThresholdInput();
-    const list = this.state.list;
-    const comparisons = list.map(this.getComparison);
+    const Content = this.state.tab ? DisplayDatas : Comparasion;
 
     return (
       <div className="App">
@@ -133,18 +23,15 @@ class App extends Component {
           <h1 className="App-title">FuzzyWuzzy String compare</h1>
         </header>
         <div className="App-intro">
-          <div>
-            <span className="Ul-param">{paramInputs}</span>
-            {thresholdInput}
-            <button onClick={() => {
-              this.setState({
-                list: [...list, DEFAULT_COM_OBJ],
-              });
-            }}>
-              Add new comparison
-            </button>
-          </div>
-          <ul className="Ul-compare">{comparisons}</ul>
+          <button onClick={() => {
+            this.setState({
+              ...this.state,
+              tab: !this.state.tab,
+            });
+          }}>
+            Swap Tab
+          </button>
+          <Content />
         </div>
       </div>
     );
