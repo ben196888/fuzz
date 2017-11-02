9a909e11006e75a8b103c4d8fc79efd5c7be55f5
Ben Liu <ben.liu@tabcorp.com.au>
2017-11-03 10:32:00 +1100

    Add DisplatDatas module for MVP selection

--- src/DisplayDatas.js
+++ src/DisplayDatas.js
@@ -1,63 +1,65 @@
 import React, { Component } from 'react';
-import { getSSData, getRSData } from './getData';
+import genTeamTuples from './genTeamTuples';
+import getCombinations from './getCombinations';
+import { getRSSelectionList, getSSSelectionList } from './getSelectionList';
+
+const TEAM_TUPLES = genTeamTuples();
+const TEAM_TUPLES_SIZE = TEAM_TUPLES.length;
+const POOL_SIZE = 1;
+const EVENT_SIZE = 1;
 
 class DisplayDatas extends Component {
   constructor() {
     super();
-    this.getDisplayEventFromObj = this.getDisplayEventFromObj.bind(this);
 
     this.state = {
-      SSData: {},
-      RSData: {},
+      pool: [],
+      SSEvents: [],
+      RSEvents: [],
     };
   }
 
-  componentDidMount() {
-    const SSData = getSSData();
-    const RSData = getRSData();
-    this.setState({
-      ...this.state,
-      SSData,
-      RSData,
-    });
-  }
-
-  getDisplayEventFromObj(eventObj) {
-    const markets = eventObj.Markets || [];
-    const eventName = <div>{eventObj.FixtureName}</div>;
-    const combinedSelectionNames = markets.map(market => {
-      const marketName = market.Name;
-      const selections = market.Selections || [];
-      const selectionNames = selections.map(selection => {
-        const selectionName = selection.Name;
-        return <div key={selection.Id}>{`${marketName} ${selectionName}`}</div>
-      });
-      return (
-        <div key={market.Id}>
-          {selectionNames}
-        </div>
-      )
-    });
-
-    return (
-      <div>
-        {eventName}
-        {combinedSelectionNames}
-      </div>
-    )
-  }
-
-
   render() {
-    const SSEventObj = this.state.SSData;
-    const RSEventObj = this.state.RSData;
-    const SSDisplay = this.getDisplayEventFromObj(SSEventObj);
-    const RSDisplay = this.getDisplayEventFromObj(RSEventObj);
+    const RSEvents = this.state.RSEvents;
+    const SSEvents = this.state.SSEvents;
+    const RSTeamTuples = RSEvents.map(v => TEAM_TUPLES[v]);
+    const SSTeamTuples = SSEvents.map(v => TEAM_TUPLES[v]);
+
+    console.log(RSTeamTuples.map(teamTuple => getRSSelectionList(teamTuple)));
+    console.log(SSTeamTuples.map(teamTuple => getSSSelectionList(teamTuple)));
 
     return (
       <div>
-        <div style={{float: 'left'}}>{SSDisplay}</div>
-        <div style={{float: 'right'}}>{RSDisplay}</div>
+        <button onClick={() => {
+          const pool = getCombinations(TEAM_TUPLES_SIZE, POOL_SIZE);
+          this.setState({
+            ...this.state,
+            pool,
+            SSEvents: [],
+            RSEvents: [],
+          });
+        }}>
+          Get Pool Candidates
+        </button>
+        <button onClick={() => {
+          const pool = this.state.pool;
+          if (pool.length !== POOL_SIZE) return;
+          const RS = getCombinations(POOL_SIZE, EVENT_SIZE);
+          const SS = getCombinations(POOL_SIZE, EVENT_SIZE);
+          const RSEvents = RS.map(v => pool[v]);
+          const SSEvents = SS.map(v => pool[v]);
+          this.setState({
+            ...this.state,
+            RSEvents,
+            SSEvents,
+          });
+        }}>
+          Get RS and SS selections
+        </button>
+        <div>{`Team Tuples size: ${TEAM_TUPLES_SIZE}, Pool size: ${POOL_SIZE}, Selection size: ${EVENT_SIZE}`}</div>
+        <div>{this.state.pool.reduce((s, v) => `${s}${v}, `, 'Pool Candidates: ')}</div>
+        <div>{this.state.RSEvents.reduce((s, v) => `${s}${v}, `, 'RSEvents: ')}</div>
+        <div>{this.state.SSEvents.reduce((s, v) => `${s}${v}, `, 'SSEvents: ')}</div>
       </div>
     );
   }
