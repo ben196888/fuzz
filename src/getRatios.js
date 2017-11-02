ad3b3ae0847ab63fcd335a6ba7531d526c07bd75
Ben Liu <ben.liu@tabcorp.com.au>
2017-11-02 14:54:24 +1100

    Add options for fuzzfall.

--- src/getRatios.js
+++ src/getRatios.js
@@ -1,13 +1,19 @@
 import fuzz from 'fuzzball';
 
-function getRatios(string1, string2) {
-  const sr = fuzz.ratio(string1, string2);
-  const pr = fuzz.partial_ratio(string1, string2);
-  const tst = fuzz.token_set_ratio(string1, string2);
-  const tsr = fuzz.token_sort_ratio(string1, string2);
-  const d = fuzz.distance(string1, string2);
+const OPTIONS = {
+  full_process: true,
+  force_ascii: false,
+};
 
-  return { sr, pr, tst, tsr, d };
+function getRatios(string1, string2) {
+  const sr = fuzz.ratio(string1, string2, OPTIONS);
+  const pr = fuzz.partial_ratio(string1, string2, OPTIONS);
+  const tst = fuzz.token_set_ratio(string1, string2, OPTIONS);
+  const tsr = fuzz.token_sort_ratio(string1, string2, OPTIONS);
+  const ptsr = fuzz.partial_token_sort_ratio(string1, string2, OPTIONS);
+  const d = fuzz.distance(string1, string2, OPTIONS);
+
+  return { sr, pr, tst, tsr, ptsr, d };
 };
 
 export default getRatios;
